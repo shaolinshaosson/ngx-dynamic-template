ae92b07769a826eaadfc26c0c862fa7473be50b1
apoterenko <a.poterenko@gmail.com>
2016-09-12 19:48:03 +0300

    *added DynamicComponentModule

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export {DynamicComponent, DynamicComponentMetadata, IComponentInputData} from './DynamicComponent';
 export {IComponentRemoteTemplateFactory} from './IComponentRemoteTemplateFactory';
+export {DynamicComponentModule} from './DynamicComponentModule';
