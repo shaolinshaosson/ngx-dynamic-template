cc6f65a78e86b5c7a8156f28784f6e9684bcdc63
apoterenko <a.poterenko@gmail.com>
2017-12-02 23:24:23 +0300

    *tslint

--- src/dynamic.base.ts
+++ src/dynamic.base.ts
@@ -49,217 +49,217 @@ export class DynamicBase implements OnChanges, OnDestroy {
   @Input() styles: string[];
   @Input() defaultTemplate: string;
 
-	private lazyExtraModules: (AnyT|Function)[] = [];
-	private injector: Injector;
-	private dynamicSelector:string;
-	private cachedDynamicModule:AnyT;
-	private cachedTemplatePlaceholder:Type<IDynamicTemplatePlaceholder>;
-	private templatePlaceholder: ComponentRef<IDynamicTemplatePlaceholder>;
-	private moduleInstance: NgModuleRef<any>;
+  private lazyExtraModules: Array<AnyT | Function> = [];
+  private injector: Injector;
+  private dynamicSelector: string;
+  private cachedDynamicModule: AnyT;
+  private cachedTemplatePlaceholder: Type<IDynamicTemplatePlaceholder>;
+  private templatePlaceholder: ComponentRef<IDynamicTemplatePlaceholder>;
+  private moduleInstance: NgModuleRef<any>;
 
-	constructor(protected dynamicExtraModules: Array<any>,
-	            protected dynamicResponseRedirectStatuses: number[],
-	            protected viewContainer: ViewContainerRef,
-	            protected compiler: Compiler,
-	            protected http: HttpClient,
-	            protected dynamicCache: DynamicCache,
-	            protected moduleFactoryLoader: NgModuleFactoryLoader,
-	            protected routes: ILazyRoute[],
-	            dynamicSelector: string) {
-		this.templateReady = new EventEmitter<IDynamicTemplatePlaceholder>();
-		this.dynamicSelector = Utils.buildByNextId(dynamicSelector);
+  constructor(protected dynamicExtraModules: any[],
+              protected dynamicResponseRedirectStatuses: number[],
+              protected viewContainer: ViewContainerRef,
+              protected compiler: Compiler,
+              protected http: HttpClient,
+              protected dynamicCache: DynamicCache,
+              protected moduleFactoryLoader: NgModuleFactoryLoader,
+              protected routes: ILazyRoute[],
+              dynamicSelector: string) {
+    this.templateReady = new EventEmitter<IDynamicTemplatePlaceholder>();
+    this.dynamicSelector = Utils.buildByNextId(dynamicSelector);
 
-		this.injector = Injector.create([], this.viewContainer.parentInjector);
-	}
+    this.injector = Injector.create([], this.viewContainer.parentInjector);
+  }
 
-	/**
-	 * @override
-	 */
-	public ngOnChanges(changes: SimpleChanges) {
-		this.ngOnDestroy();
+  /**
+   * @override
+   */
+  public ngOnChanges(changes: SimpleChanges) {
+    this.ngOnDestroy();
 
-		// TODO investigate memory leak in the specific case
-		this.buildModule().then((module: AnyT) => {
-				let compiledModule: Promise<ModuleWithComponentFactories<any>>;
-				const currentModuleHash: string = Reflect.get(module, HASH_FIELD);
+    // TODO investigate memory leak in the specific case
+    this.buildModule().then((module) => {
+        let compiledModule: Promise<ModuleWithComponentFactories<any>>;
+        const currentModuleHash: string = Reflect.get(module, HASH_FIELD);
 
-				if (Utils.isPresent(currentModuleHash)) {
-					compiledModule = this.dynamicCache.get(currentModuleHash);
-					if (!Utils.isPresent(compiledModule)) {
-						this.dynamicCache.set(currentModuleHash, compiledModule = this.compiler.compileModuleAndAllComponentsAsync<any>(module));
-					}
-				} else {
-					compiledModule = this.compiler.compileModuleAndAllComponentsAsync<any>(module);
-				}
+        if (Utils.isPresent(currentModuleHash)) {
+          compiledModule = this.dynamicCache.get(currentModuleHash);
+          if (!Utils.isPresent(compiledModule)) {
+            this.dynamicCache.set(currentModuleHash, compiledModule = this.compiler.compileModuleAndAllComponentsAsync<any>(module));
+          }
+        } else {
+          compiledModule = this.compiler.compileModuleAndAllComponentsAsync<any>(module);
+        }
 
-				compiledModule
-					.then((compiledModule: ModuleWithComponentFactories<any>) => this.makeDynamicTemplatePlaceholder(compiledModule))
-			}
-		);
-	}
+        compiledModule
+          .then((compiledModule) => this.makeDynamicTemplatePlaceholder(compiledModule));
+      }
+    );
+  }
 
-	/**
-	 * @override
-	 */
-	public ngOnDestroy() {
-		if (Utils.isPresent(this.moduleInstance)) {
-			this.moduleInstance.destroy();
-			this.moduleInstance = null;
-		}
-		if (Utils.isPresent(this.templatePlaceholder)) {
-			this.templatePlaceholder.destroy();
-			this.templatePlaceholder = null;
-		}
-		if (Utils.isPresent(this.cachedDynamicModule)) {
-			this.compiler.clearCacheFor(this.cachedDynamicModule);
-			this.cachedDynamicModule = null;
-		}
-		if (Utils.isPresent(this.cachedTemplatePlaceholder)) {
-			this.compiler.clearCacheFor(this.cachedTemplatePlaceholder);
-			this.cachedTemplatePlaceholder = null;
-		}
-	}
+  /**
+   * @override
+   */
+  public ngOnDestroy() {
+    if (Utils.isPresent(this.moduleInstance)) {
+      this.moduleInstance.destroy();
+      this.moduleInstance = null;
+    }
+    if (Utils.isPresent(this.templatePlaceholder)) {
+      this.templatePlaceholder.destroy();
+      this.templatePlaceholder = null;
+    }
+    if (Utils.isPresent(this.cachedDynamicModule)) {
+      this.compiler.clearCacheFor(this.cachedDynamicModule);
+      this.cachedDynamicModule = null;
+    }
+    if (Utils.isPresent(this.cachedTemplatePlaceholder)) {
+      this.compiler.clearCacheFor(this.cachedTemplatePlaceholder);
+      this.cachedTemplatePlaceholder = null;
+    }
+  }
 
-	private makeDynamicTemplatePlaceholder(moduleWithComponentFactories: ModuleWithComponentFactories<any>) {
-		this.moduleInstance = moduleWithComponentFactories.ngModuleFactory.create(this.injector);
+  private makeDynamicTemplatePlaceholder(moduleWithComponentFactories: ModuleWithComponentFactories<any>) {
+    this.moduleInstance = moduleWithComponentFactories.ngModuleFactory.create(this.injector);
 
-		const factory = moduleWithComponentFactories.componentFactories.find((componentFactory: ComponentFactory<AnyT>) => {
-				return componentFactory.selector === this.dynamicSelector
-					|| (Utils.isPresent(componentFactory.componentType) && Utils.isPresent(this.template)
-					&& Reflect.get(componentFactory.componentType, HASH_FIELD) === Utils.hashFnv32a(this.template, true));
-			}
-		);
+    const factory = moduleWithComponentFactories.componentFactories.find((componentFactory: ComponentFactory<AnyT>) => {
+        return componentFactory.selector === this.dynamicSelector
+          || (Utils.isPresent(componentFactory.componentType) && Utils.isPresent(this.template)
+            && Reflect.get(componentFactory.componentType, HASH_FIELD) === Utils.hashFnv32a(this.template, true));
+      }
+    );
 
-		const templatePlaceholder = this.templatePlaceholder = factory.create(this.injector, null, null, this.moduleInstance);
-		this.viewContainer.insert(templatePlaceholder.hostView, 0);
-		this.applyPropertiesToDynamicTemplatePlaceholder(this.templatePlaceholder.instance);
+    const templatePlaceholder = this.templatePlaceholder = factory.create(this.injector, null, null, this.moduleInstance);
+    this.viewContainer.insert(templatePlaceholder.hostView, 0);
+    this.applyPropertiesToDynamicTemplatePlaceholder(this.templatePlaceholder.instance);
 
-		this.templateReady.emit(this.templatePlaceholder.instance);
-	}
+    this.templateReady.emit(this.templatePlaceholder.instance);
+  }
 
-	private buildModule(): Promise<AnyT> {
-		const lazyModules: string[] = [].concat(this.lazyModules || []);
-		const lazyModulesLoaders: Array<Promise<NgModuleFactory<any>|Function>> = [];
+  private buildModule(): Promise<AnyT> {
+    const lazyModules: string[] = [].concat(this.lazyModules || []);
+    const lazyModulesLoaders: Array<Promise<NgModuleFactory<any> | Function>> = [];
 
-		for (let lazyModule of lazyModules) {
-			const lazyRoute: ILazyRoute = Utils.findLazyRouteLoader(lazyModule, this.routes);
-			if (lazyRoute) {
-				if (Utils.isFunction(lazyRoute.loadChildren)) {
-					// angular2-class starter
-					lazyModulesLoaders.push(
-						Observable.of((lazyRoute.loadChildren as Function)()).toPromise()
-					);
-				} else {
-					// angular-cli
-					lazyModulesLoaders.push(this.moduleFactoryLoader.load(lazyRoute.loadChildren as string));
-				}
-			} else {
-				lazyModulesLoaders.push(this.moduleFactoryLoader.load(lazyModule));
-			}
-		}
-		return new Promise((resolve: (value: AnyT) => void) => {
-			Promise.all(lazyModulesLoaders)
-				.then((moduleFactories: (NgModuleFactory<any>|Function)[]) => {
-					for (let moduleFactory of moduleFactories) {
-						if (moduleFactory instanceof NgModuleFactory) {
-							// angular-cli
-							this.lazyExtraModules.push(moduleFactory.moduleType);
-						} else {
-							// angular2-class starter
-							this.lazyExtraModules.push(moduleFactory);
-						}
-					}
-					if (Utils.isPresent(this.template)) {
-						resolve(this.makeComponentModule({template: this.template}));
-					} else if (Utils.isPresent(this.httpUrl)) {
-						this.loadRemoteTemplate(this.httpUrl, resolve);
-					} else {
-						resolve(this.makeComponentModule());
-					}
-				});
-		});
-	}
+    for (const lazyModule of lazyModules) {
+      const lazyRoute: ILazyRoute = Utils.findLazyRouteLoader(lazyModule, this.routes);
+      if (lazyRoute) {
+        if (Utils.isFunction(lazyRoute.loadChildren)) {
+          // angular2-class starter
+          lazyModulesLoaders.push(
+            Observable.of((lazyRoute.loadChildren as Function)()).toPromise()
+          );
+        } else {
+          // angular-cli
+          lazyModulesLoaders.push(this.moduleFactoryLoader.load(lazyRoute.loadChildren as string));
+        }
+      } else {
+        lazyModulesLoaders.push(this.moduleFactoryLoader.load(lazyModule));
+      }
+    }
+    return new Promise((resolve: (value: AnyT) => void) => {
+      Promise.all(lazyModulesLoaders)
+        .then((moduleFactories: Array<NgModuleFactory<any> | Function>) => {
+          for (const moduleFactory of moduleFactories) {
+            if (moduleFactory instanceof NgModuleFactory) {
+              // angular-cli
+              this.lazyExtraModules.push(moduleFactory.moduleType);
+            } else {
+              // angular2-class starter
+              this.lazyExtraModules.push(moduleFactory);
+            }
+          }
+          if (Utils.isPresent(this.template)) {
+            resolve(this.makeComponentModule({ template: this.template }));
+          } else if (Utils.isPresent(this.httpUrl)) {
+            this.loadRemoteTemplate(this.httpUrl, resolve);
+          } else {
+            resolve(this.makeComponentModule());
+          }
+        });
+    });
+  }
 
-	private loadRemoteTemplate(httpUrl: string, resolve: (value: AnyT) => void) {
-		let requestArgs: { [index: string]: any; } = {withCredentials: true};
-		if (Utils.isPresent(this.remoteTemplateFactory)
-			&& Utils.isFunction(this.remoteTemplateFactory.buildRequestOptions)) {
-			requestArgs = this.remoteTemplateFactory.buildRequestOptions();
-		}
+  private loadRemoteTemplate(httpUrl: string, resolve: (value: AnyT) => void) {
+    let requestArgs: { [index: string]: any; } = { withCredentials: true };
+    if (Utils.isPresent(this.remoteTemplateFactory)
+      && Utils.isFunction(this.remoteTemplateFactory.buildRequestOptions)) {
+      requestArgs = this.remoteTemplateFactory.buildRequestOptions();
+    }
 
-		this.http.get(httpUrl, requestArgs)
-			.subscribe((response: Response) => {
-				if (this.dynamicResponseRedirectStatuses.indexOf(response.status) > -1) {
-					const chainedUrl: string = response.headers.get('Location');
-					if (Utils.isPresent(chainedUrl)) {
-						this.loadRemoteTemplate(chainedUrl, resolve);
-					}
-				} else {
-					const loadedTemplate = Utils.isPresent(this.remoteTemplateFactory)
-					&& Utils.isFunction(this.remoteTemplateFactory.parseResponse)
-						? this.remoteTemplateFactory.parseResponse(response)
-						: response.text();
+    this.http.get(httpUrl, requestArgs)
+      .subscribe((response: Response) => {
+        if (this.dynamicResponseRedirectStatuses.indexOf(response.status) > -1) {
+          const chainedUrl: string = response.headers.get('Location');
+          if (Utils.isPresent(chainedUrl)) {
+            this.loadRemoteTemplate(chainedUrl, resolve);
+          }
+        } else {
+          const loadedTemplate = Utils.isPresent(this.remoteTemplateFactory)
+          && Utils.isFunction(this.remoteTemplateFactory.parseResponse)
+            ? this.remoteTemplateFactory.parseResponse(response)
+            : response.text();
 
-					resolve(this.makeComponentModule({template: loadedTemplate as string}));
-				}
-			}, () => {
-				const template: string = this.defaultTemplate || '';
-				resolve(this.makeComponentModule({template: template}));
-			});
-	}
+          resolve(this.makeComponentModule({ template: loadedTemplate as string }));
+        }
+      }, () => {
+        const template: string = this.defaultTemplate || '';
+        resolve(this.makeComponentModule({ template }));
+      });
+  }
 
-	private makeComponentModule(dynamicConfig?: IDynamicComponentConfig): AnyT {
-		const dynamicComponentType: Type<IDynamicTemplatePlaceholder>
-			= this.cachedTemplatePlaceholder
-			= this.makeComponent(dynamicConfig);
+  private makeComponentModule(dynamicConfig?: IDynamicComponentConfig): AnyT {
+    const dynamicComponentType: Type<IDynamicTemplatePlaceholder>
+      = this.cachedTemplatePlaceholder
+      = this.makeComponent(dynamicConfig);
 
-		const modules: any[] = this.dynamicExtraModules
-			.concat(this.extraModules || [])
-			.concat(this.lazyExtraModules);
+    const modules: any[] = this.dynamicExtraModules
+      .concat(this.extraModules || [])
+      .concat(this.lazyExtraModules);
 
-		@NgModule({
-			declarations: [dynamicComponentType],
-			imports: [CommonModule].concat(modules)
-		})
-		class dynamicComponentModule {
-		}
+    @NgModule({
+      declarations: [dynamicComponentType],
+      imports: [CommonModule].concat(modules),
+    })
+    class DynamicComponentModule {
+    }
 
-		const dynamicComponentTypeHash: string = Reflect.get(dynamicComponentType, HASH_FIELD);
-		if (Utils.isPresent(dynamicComponentTypeHash)) {
-			Reflect.set(dynamicComponentModule, HASH_FIELD, dynamicComponentTypeHash);
-		}
-		return this.cachedDynamicModule = dynamicComponentModule;
-	}
+    const dynamicComponentTypeHash: string = Reflect.get(dynamicComponentType, HASH_FIELD);
+    if (Utils.isPresent(dynamicComponentTypeHash)) {
+      Reflect.set(DynamicComponentModule, HASH_FIELD, dynamicComponentTypeHash);
+    }
+    return this.cachedDynamicModule = DynamicComponentModule;
+  }
 
-	private makeComponent(componentConfig?: IDynamicComponentConfig): Type<IDynamicTemplatePlaceholder> {
-		const dynamicComponentMetaData: IDynamicTemplateMetadata = {
-			selector: this.dynamicSelector,
-			styles: this.styles
-		};
+  private makeComponent(componentConfig?: IDynamicComponentConfig): Type<IDynamicTemplatePlaceholder> {
+    const dynamicComponentMetaData: IDynamicTemplateMetadata = {
+      selector: this.dynamicSelector,
+      styles: this.styles,
+    };
 
-		if (Utils.isPresent(componentConfig)) {
-			if (Utils.isPresent(componentConfig.template)) {
-				dynamicComponentMetaData.template = componentConfig.template;
-			} else if (Utils.isPresent(componentConfig.templatePath)) {
-				dynamicComponentMetaData.templateUrl = componentConfig.templatePath;
-			}
-		}
+    if (Utils.isPresent(componentConfig)) {
+      if (Utils.isPresent(componentConfig.template)) {
+        dynamicComponentMetaData.template = componentConfig.template;
+      } else if (Utils.isPresent(componentConfig.templatePath)) {
+        dynamicComponentMetaData.templateUrl = componentConfig.templatePath;
+      }
+    }
 
-		@Component(dynamicComponentMetaData)
-		class dynamicComponentClass {
-		}
+    @Component(dynamicComponentMetaData)
+    class DynamicComponentClass {
+    }
 
-		const templateMetaData = Reflect.get(dynamicComponentMetaData, 'template');
-		if (Utils.isPresent(templateMetaData)) {
-			Reflect.set(dynamicComponentClass, HASH_FIELD, Utils.hashFnv32a(templateMetaData, true));
-		}
-		return dynamicComponentClass as Type<IDynamicTemplatePlaceholder>;
-	}
+    const templateMetaData = Reflect.get(dynamicComponentMetaData, 'template');
+    if (Utils.isPresent(templateMetaData)) {
+      Reflect.set(DynamicComponentClass, HASH_FIELD, Utils.hashFnv32a(templateMetaData, true));
+    }
+    return DynamicComponentClass as Type<IDynamicTemplatePlaceholder>;
+  }
 
-	private applyPropertiesToDynamicTemplatePlaceholder(instance: IDynamicTemplatePlaceholder) {
-		if (!Utils.isPresent(this.context)) {
-			return;
-		}
-		Utils.applySourceAttributes(instance, this.context);
-	}
+  private applyPropertiesToDynamicTemplatePlaceholder(instance: IDynamicTemplatePlaceholder) {
+    if (!Utils.isPresent(this.context)) {
+      return;
+    }
+    Utils.applySourceAttributes(instance, this.context);
+  }
 }
--- src/dynamic.module.ts
+++ src/dynamic.module.ts
@@ -5,27 +5,30 @@ import { DynamicCache } from './dynamic.cache';
 import { DynamicTypes, IDynamicTemplateOptions, ROUTES_TOKEN } from './dynamic.interface';
 
 @NgModule(
-	{
-		declarations: [
-			DynamicDirective
-		],
-		exports: [
-			DynamicDirective
-		]
-	}
+  {
+    declarations: [
+      DynamicDirective
+    ],
+    exports: [
+      DynamicDirective
+    ],
+  }
 )
 export class NgxDynamicTemplateModule {
 
-	static forRoot(options?: IDynamicTemplateOptions): ModuleWithProviders {
-		return {
-			ngModule: NgxDynamicTemplateModule,
-			providers: [
-				DynamicCache,
-				{ provide: DynamicTypes.DynamicExtraModules, useValue: options && options.extraModules ? options.extraModules : [] },
-				{ provide: DynamicTypes.DynamicResponseRedirectStatuses, useValue: [301, 302, 307, 308] },
-				{ provide: NgModuleFactoryLoader, useClass: SystemJsNgModuleLoader },
-				{ provide: ROUTES_TOKEN, useValue: options.routes || [] }
-			]
-		};
-	}
+  public static forRoot(options?: IDynamicTemplateOptions): ModuleWithProviders {
+    return {
+      ngModule: NgxDynamicTemplateModule,
+      providers: [
+        DynamicCache,
+        {
+          provide: DynamicTypes.DynamicExtraModules,
+          useValue: options && options.extraModules ? options.extraModules : [],
+        },
+        { provide: DynamicTypes.DynamicResponseRedirectStatuses, useValue: [301, 302, 307, 308] },
+        { provide: NgModuleFactoryLoader, useClass: SystemJsNgModuleLoader },
+        { provide: ROUTES_TOKEN, useValue: options.routes || [] }
+      ],
+    };
+  }
 }
